fb982deabb048f2420bcd80a07bc83115cd85f3d
Dantesakt1 <dvntecastillo@gmail.com>
2026-05-05 01:43:43 -0400

    actualizacion pequeña

--- gestion_animales/src/main/java/com/example/demo/controller/VentaController.java
+++ gestion_animales/src/main/java/com/example/demo/controller/VentaController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import org.apache.kafka.common.protocol.types.Field.Str;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
