dcebb6900a873043ced3a24623e74f4e0da676e5
elpepesech12 <fe.lopezl@gmail.com>
2026-05-10 20:34:50 -0400

    Actualizacion gestion usuarios

---
+++ gestion_usuarios/src/main/java/com/example/gestion_usuarios/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.example.gestion_usuarios.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.gestion_usuarios.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
